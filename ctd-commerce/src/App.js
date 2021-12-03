cb6db4e1525aabf9f05b02053f2dcce6accc7dd0
Leonardo Martins <leodsmartins@gmail.com>
2021-12-03 14:37:58 -0300

    Ajustes

--- ctd-commerce/src/App.js
+++ ctd-commerce/src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import './App.scss';
 import React from 'react';
 import Header from './Components/Header';
 import Carousel from './Components/Carousel';
+
 
 function App() {
   return (
     <>
     <Header />
-    <Carousel />
+    <Carousel />  
     </>
   );
 }
