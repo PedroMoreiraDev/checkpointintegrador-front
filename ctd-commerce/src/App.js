a505d29254373d76550e752f51fcb2d927c97efd
Leonardo Martins <leodsmartins@gmail.com>
2021-12-03 01:50:23 -0300

    Testando cards e carousel

--- ctd-commerce/src/App.js
+++ ctd-commerce/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
 import Header from './Components/Header';
+import Carousel from './Components/Carousel';
 
 function App() {
   return (
     <>
     <Header />
+    <Carousel />
     </>
   );
 }
