7304a140fe772ddfb28d552cba0abed5c07e5fdc
Leonardo Martins <leodsmartins@gmail.com>
2021-12-07 19:12:23 -0300

    Correção da responsividade do carousel usando react-bootstrap

--- ctd-commerce/src/pages/Home/Components/Body/index.js
+++ ctd-commerce/src/pages/Home/Components/Body/index.js
@@ -5,26 +5,7 @@ import CardProd from '../Card';
 
 const Body =()=> {
 return (
-    <>
-        <div class="container-fluid d-flex justify-content-center align-itens-center" id="container-body">
-            <div class="d-flex justify-content-center" id="teste-1">Teste1</div> 
-
-            <div class="d-flex justify-content-center" id="teste-2">
-                <div class="d-flex justify-content-center" id="products"><h2>Os melhores produtos da galaxia</h2></div>
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />
-                <CardProd />               
-            </div>
-        
-           
-        </div>
+<>
 </> 
 )
 
