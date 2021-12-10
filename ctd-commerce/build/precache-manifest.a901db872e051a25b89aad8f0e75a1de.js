2c475e9f65653bbdacd113377273cfa1b3f60231
Fer96carvalho <Fer96carvalho@root>
2021-12-10 19:12:11 -0400

    pasta build

---
+++ ctd-commerce/build/precache-manifest.a901db872e051a25b89aad8f0e75a1de.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49983e71ddb49204a50bed31f0e49607",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d782fecd7bf245190bca",
+    "url": "/static/css/main.6666e1f2.chunk.css"
+  },
+  {
+    "revision": "f709b331b44b78740783",
+    "url": "/static/js/2.4e5e7001.chunk.js"
+  },
+  {
+    "revision": "e445ba647b97a23b6382102dfc5ce3d2",
+    "url": "/static/js/2.4e5e7001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d782fecd7bf245190bca",
+    "url": "/static/js/main.9a008632.chunk.js"
+  },
+  {
+    "revision": "ee596050ed97db6717a4",
+    "url": "/static/js/runtime-main.1776f711.js"
+  },
+  {
+    "revision": "7a25eb37f1275712444b0ab3b3311502",
+    "url": "/static/media/banner-rocket.7a25eb37.png"
+  },
+  {
+    "revision": "3e0f620dd358829fb6906d9993e86d8c",
+    "url": "/static/media/cupom.3e0f620d.png"
+  },
+  {
+    "revision": "797ba69699645eb2effaf825b805b44d",
+    "url": "/static/media/duy.797ba696.png"
+  },
+  {
+    "revision": "d6fbad32efe979527498c49eac3c1b2e",
+    "url": "/static/media/fernando.d6fbad32.png"
+  },
+  {
+    "revision": "0264fea9bc791d4f6b4f98cda62be463",
+    "url": "/static/media/galaxy-stars.0264fea9.png"
+  },
+  {
+    "revision": "0090cd68fe750722e86edece736bf914",
+    "url": "/static/media/leonardo.0090cd68.png"
+  },
+  {
+    "revision": "197172ead967008b47f7390cecad8227",
+    "url": "/static/media/mariana.197172ea.png"
+  },
+  {
+    "revision": "249f034ba1ea039c314f195147caa8ea",
+    "url": "/static/media/nave.249f034b.png"
+  },
+  {
+    "revision": "0eaf03cd59ba5f508a547754842515d0",
+    "url": "/static/media/pedro.0eaf03cd.png"
+  },
+  {
+    "revision": "d0a24811ac859b4d47a337674f974883",
+    "url": "/static/media/peter.d0a24811.png"
+  },
+  {
+    "revision": "87095846eca562eeb25690702a0e88e4",
+    "url": "/static/media/rocket-purple-light.87095846.png"
+  },
+  {
+    "revision": "a372e7ecba48376b5db451689770d293",
+    "url": "/static/media/startup.a372e7ec.png"
+  }
+]);
